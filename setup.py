c236f9c783090068e3b83a13c1a1fa8f277fc27e
Cedric Zhuang <jealous@163.com>
2020-02-02 02:30:20 +0800

    [GH-50] Pandas 1.0 compatible issue. (#51)
    Pandas 1.0 introduces a `_ensure_type` method that asserts the type of
    `self` must be the same type as the input `obj`.  In this lib, the type
    of `self` is `StockDataFrame` and the type of the object is `DataFrame`
    which breaks this assert.
    
    The temporary fix is to bypass the assert by override the `_ensure_type`
    method in `StockDataFrame`.  The is just a work around.  The good news
    is that the original method doesn't contain any business logic.
    
    Other changes:
    * Update the travis configuration to add CI for python 3.8.
    * Drop the CI build for 3.4.  It's too old and takes too long to run.

--- setup.py
+++ setup.py
@@ -80,9 +80,10 @@ setup(
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: CPython",
         "Natural Language :: English",
         "Intended Audience :: Developers",
